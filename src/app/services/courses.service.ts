0fc6e9b6d41fbd8af6e9203b42209808a6bc251a
Kevin Wong <kevin.wong1@gmail.com>
2019-10-05 16:54:24 -0400

    Sec 3-22: AngCustom Service - Data Mod w/ HTTP put

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Course } from '../model/course';
 
@@ -17,4 +17,11 @@ export class CoursesService {
     return this.http.get<Course[]>('/api/courses', { params });
 
   }
+
+  saveCourse(course: Course) {
+    const headers = new HttpHeaders()
+      .set("X-Auth", "userId");
+    return this.http.put(`/api/courses/${course.id}`, course, { headers });
+  }
+
 }
